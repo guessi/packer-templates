ebecae25d55dc63a180ce13d2616ff7690322581
guessi <guessi@gmail.com>
2018-01-14 15:57:36 +0800

    don't overwrite existed version

--- setup.sh
+++ setup.sh
@@ -10,12 +10,12 @@ if [ -d "/opt/packer" ]; then
   exit 1
 fi
 
-PACKER_VERSION="${PACKER_VERSION:-1.1.2}"
-if [ -x "/opt/packer" ] && [ "${PACKER_VERSION}" = "$(/opt/packer --version)" ]; then
-  echo "Skip, packer ${PACKER_VERSION} has already existed"
+if [ -x "/opt/packer" ]; then
+  echo "Skip, packer has already existed (Version: "$(/opt/packer --version)")"
   exit 0
 fi
 
+PACKER_VERSION="${PACKER_VERSION:-1.1.2}"
 echo "Setup packer ${PACKER_VERSION} in process... "
 wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip -qO /tmp/packer.zip
 unzip -q -o -d /opt /tmp/packer.zip
